a05b2a9a665da41cacfec4b9d50c649312334ae2
Naghulram C <145176091+naghul30@users.noreply.github.com>
2025-10-14 18:49:38 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,19 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Microsoft Clarity Tracking Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tq1wa1u5e0");
+            `,
+          }}
+        />
+
         {/* Hotjar Placeholder */}
         <script
           dangerouslySetInnerHTML={{
