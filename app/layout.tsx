f25b3bf3d7eb139d3decd7cd05ea3333c735ca21
Naghulram C <145176091+naghul30@users.noreply.github.com>
2025-10-15 23:02:36 +0530

    latest

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,9 +44,22 @@ export default function RootLayout({
             `,
           }}
         />
-        {/* Contentsquare */} 
-        <script src="https://t.contentsquare.net/uxa/480645169b6a9.js"> 
-        </script>  
+        {/* Hotjar Tracking Code for https://gameversea.netlify.app/ */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:6547989,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+
 
       </head>
       <body className={`${inter.className} bg-gray-950 text-white min-h-screen`}>
